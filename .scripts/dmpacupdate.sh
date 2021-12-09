61332c019c48d7f8778f3308cc1362e152290ffb
Nguyen Le Tien Trien <trien19012004@gmail.com>
2021-12-09 23:04:26 +0700

    dmpacupdate.sh

--- .scripts/dmpacupdate.sh
+++ .scripts/dmpacupdate.sh
@@ -35,7 +35,8 @@ help_h() {
 run() {
 # Run
     update="$(checkupdates | wc -l)"
-    if [[ $update != 0 ]] && [[ $(echo -e "Yes\nNo" | dmenu -i -p "$update packages can be updated! Do you want to update?") = "Yes" ]]; then
+    yn="$(echo -e "Yes\nNo" | dmenu -i -p "$update packages can be updated! Do you want to update?")"
+    if [[ $update != 0 ]] && [[ "$yn" = "Yes" ]]; then
         $1 sh -c "sudo pacman -Syu && echo -e '\nSuccessfully!'; echo -e '\nPress ENTER to quit.' && read"
         [[ "$?" != "0" ]] && notify-send "Check for updates script" "Command '$1' is not correct to run."
     else

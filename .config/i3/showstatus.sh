c1c6286205b6ac757806e6ebb4a1d8992a9809ad
Nguyen Le Tien Trien <trien19012004@gmail.com>
2021-12-10 20:54:23 +0700

    i3bar-showstatus.sh + dunstrc

--- .config/i3/showstatus.sh
+++ .config/i3/showstatus.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
+cpu() {
+    read cpu a b c previdle rest < /proc/stat
+    prevtotal=$((a+b+c+previdle))
+    sleep 0.5
+    read cpu a b c idle rest < /proc/stat
+    total=$((a+b+c+idle))
+    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+    echo -e "$cpu%"
+}
 
+time=" $(date "+%H:%M:%S")"
+date=" $(date "+%A, %d/%m/%y")"
+update=" $(checkupdates | wc -l) packages"
+cpu="龍 $(cpu)"
+mem=" $(free -h | grep "Mem" | awk '{print $3}')"
+disk=" $(df -h | grep "/$" | awk '{print $4}')"
+volume=" $(pamixer --get-volume)%"
+
+status=$(echo -e "$time\n$date\n$update\n$cpu\n$mem\n$disk\n$volume")
+notify-send "Status" "$status"

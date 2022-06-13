900b3282ede883be2032aa7e0e9f060624b3df32
Nguyen Le Tien Trien <trien19012004@gmail.com>
2022-06-13 21:36:51 +0700

    qtile(Match): add GPG key password entry

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -495,7 +495,7 @@ floating_layout = layout.Floating(float_rules=[
     # Match(wm_class='maketag'),  # gitk
     # Match(wm_class='ssh-askpass'),  # ssh-askpass
     # Match(title='branchdialog'),  # gitk
-    # Match(title='pinentry'),  # GPG key password entry
+    Match(wm_class='pinentry-gtk-2'), # GPG key password entry
 ], **layout_theme)
 auto_fullscreen = True
 focus_on_window_activation = "smart"

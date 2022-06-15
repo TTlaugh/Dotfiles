70ca5887f8cd7dfae4c02e288aed72203adf31e7
Nguyen Le Tien Trien <trien19012004@gmail.com>
2022-06-15 11:51:59 +0700

    qtile(autostart).add: autosyncemail script

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,3 +3,7 @@
 picom &
 nm-applet &
 exec "$HOME"/.fehbg &
+xset s off && xset -dpms && xset s noblank &
+
+# Run the emailautosync script to send a message asking if the user wants to enable automatic email sync.
+setsid -f "$HOME"/.config/qtile/scripts/autosyncemail

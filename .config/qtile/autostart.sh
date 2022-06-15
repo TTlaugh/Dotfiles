bc01f3565d4d8d0198526af7e46d599f670be200
Nguyen Le Tien Trien <trien19012004@gmail.com>
2022-06-15 20:44:34 +0700

    updates

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,5 +5,5 @@ nm-applet &
 exec "$HOME"/.fehbg &
 xset s off && xset -dpms && xset s noblank &
 
-# Run the emailautosync script to send a message asking if the user wants to enable automatic email sync.
-setsid -f "$HOME"/.config/qtile/scripts/autosyncemail
+# Run the aems script to send a message asking if the user wants to enable automatic email sync.
+setsid -f "$HOME"/.config/qtile/scripts/aems

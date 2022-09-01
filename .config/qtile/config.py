e7e66c6bdd8a36d676fe3fc0d3120d8762574a17
Nguyen Le Tien Trien <trien19012004@gmail.com>
2022-09-01 11:29:40 +0700

    qtile bar size, trayer szie

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -370,7 +370,7 @@ def init_widgets_screen1():
 
 def init_screens():
     return [
-        Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20, margin=[2, 80, 0, 10],))
+        Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=23, margin=[2, 80, 0, 10],))
         #Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20))
     ]
 

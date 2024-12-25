29d99e15f00560cb0f311b34f8d1201af11240f2
Your Name <you@example.com>
2024-12-26 04:12:25 +0700

    fix pipewire startup

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2046
 
-set -e
-
 pwd=$(pwd)
 
 GitRepos="$HOME/GitRepos"
@@ -20,14 +18,22 @@ mydwm="https://github.com/TTlaugh/dwm"
 mydmenu="https://github.com/TTlaugh/dmenu"
 
 createDir() {
-    [[ ! -d "$GitRepos" ]] && mkdir -p "$GitRepos"
-    [[ ! -d "$Suckless" ]] && mkdir -p "$Suckless"
-    [[ ! -d "$tools"    ]] && mkdir -p "$tools"
+    [ ! -d "$GitRepos" ] && mkdir -p "$GitRepos"
+    [ ! -d "$Suckless" ] && mkdir -p "$Suckless"
+    [ ! -d "$tools"    ] && mkdir -p "$tools"
 }
 
 install_pacpack(){
-    # shellcheck disable=SC2046
-    sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $pkglist))
+    cd pkg && invalid_packages_name="$(./invalidname)" && cd ..
+    if [ -n "$invalid_packages_name" ]; then
+        echo "There are some packages with invalid names:"
+        echo "$invalid_packages_name"
+        read -r -p "Do you want to continue (they will be ignored)? [Y/n] " yn
+        case $yn in
+            ""|[Yy]* ) sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $pkglist)) ;;
+            * ) exit 1 ;;
+        esac
+    fi
 }
 install_graphics() {
     read -r -p "Do you want to install nvidia driver? [Y/n] " yn
@@ -72,6 +78,10 @@ copy_symblink() {
     cp -a ".xprofile" "$HOME"
     cp -a ".local/share/bg" "$HOME/.local/share/bg"
 }
+disable_services() {
+    # start pipewire (with pipewire-pulse, wireplumber) via xprofile instead of systemd
+    sudo systemctl --global disable pipewire pipewire-pulse wireplumber
+}
 enable_services() {
     printf "Please enable these services manually: \`systemctl enable ...\`
     - Networkmanager: NetworkManager.service
@@ -81,10 +91,11 @@ enable_services() {
     - Bluetooth:      bluetooth.service (Don't run if you not use Bluetooth)
 (*) Check the wiki to ensure your system can do it
 Press any key to continue..."
-    read -r
+    read -r _
 }
 
 createDir
+set -e
 install_pacpack
 install_graphics
 install_yay
@@ -96,4 +107,5 @@ sudo usermod -aG wheel,video,audio,input,optical,storage "$USER"
 printf "FONT=ter-132n" | sudo tee -a /etc/vconsole.conf
 sudo cp "misc/etc/X11/xorg.conf.d/30-touchpad.conf" "/etc/X11/xorg.conf.d/30-touchpad.conf"
 chsh -s "$(which zsh)"
+disable_services
 enable_services
